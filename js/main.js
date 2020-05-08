b4c81239903e35663135a7de424dc3a821d9168c
MUHAMMED YAZEEN <muhammedyaseen1012@gmail.com>
2020-05-08 23:08:21 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function fun1(){
+document.getElementById("img1").src="/img/icon.png ";
+};
+function fun2()
+{
+    document.getElementById("img1").src="/img/mypic.JPG";
+  
+};
+// var i=0;
+
+// function change()
+// {
+//     if(i>1){
+//         i=0;
+//     }
+//     var a=["BLOGGER","WEB DESIGNER"];
+//     document.getElementById("custext").innerHTML=a[i]
+//     i=i+1
+// };
+// window.setInterval(change, 2000);
